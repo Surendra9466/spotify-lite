51d7391747a4c076a3d2e654b3c755331a2f9d87
Surendra <ssingh@forbestech.com>
2022-01-06 11:10:24 +0530

    auth added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -14,7 +14,7 @@ const Auth = () => {
             ) : (
                 // Display the login page
                 <SpotifyAuth className="auth"
-                    redirectUri='http://localhost:3000'
+                    redirectUri={`http://localhost:3000` || 'https://awesome-hodgkin-ddc4a6.netlify.app/'}
                     clientID='761c538089074f8d806178d2045e7d21'
                     scopes={[Scopes.userReadPrivate, 'user-read-email', 'user-read-recently-played']} // either style will work
                     onAccessToken={(token) => setToken(token)}   
